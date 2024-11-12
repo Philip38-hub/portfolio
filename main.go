2c06739ff830a59337e029095e5a1d5d67dd90e5
Philip Ochieng <oumaphilip01@gmail.com>
2024-11-12 16:54:16 +0300

    final commit V1 for portfolio

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 )
 
 func main() {
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	http.HandleFunc("/", indexHandler)
 	log.Printf("Starting server at port 8080")
 	err := http.ListenAndServe(":8080", nil)
@@ -16,7 +19,7 @@ func main() {
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	tmp, err := template.ParseFiles("index2.html")
+	tmp, err := template.ParseFiles("index.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
